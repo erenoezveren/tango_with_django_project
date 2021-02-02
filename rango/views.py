4e28530814ca865b4bd1789307456acbb15a94d2
Eren Oezveren <2440801o@student.gla.ac.uk>
2021-02-02 17:01:19 +0100

    chapter 4 with errors

--- rango/views.py
+++ rango/views.py
@@ -15,4 +15,6 @@ def index(request):
     
 	 
 def about(request):
-     return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+     context_dict = {'boldmessage': 'This tutorial has been put together by Eren'}
+
+     return render(request, 'rango/about.html', context=context_dict)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+				'django.template.context_processors.media',
             ],
         },
     },
@@ -72,6 +74,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
+# Media File Hosting
+
+MEDIA_ROOT = MEDIA_DIR
+
+MEDIA_URL = '/media/'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
